8533222efc75231daf3a46a9a2bb45b0c898d2ea
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-05-16 14:36:29 -0500

    Version 1

--- dog.rb
+++ dog.rb
@@ -6,6 +6,7 @@ url = 'https://dog.ceo/api/breeds/list/all'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
+#pp dog_breeds # pp stands for pretty print.
  
 dog_breeds['message'].each do |breed, sub_breeds|
     puts " #{breed}"
--- tree.rb
+++ tree.rb
@@ -6,3 +6,12 @@ url = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json?%24limit=306000'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 all_trees = JSON.parse(response) # Convert JSON data into Ruby data.
+
+ash_count = 0
+all_trees.each do |tree|
+    if tree['common_name'] && tree['common_name'].include?('ash')
+        ash_count += 1
+    end
+end
+
+puts "Total Ash Trees in Winnipeg = #{ash_count}"
